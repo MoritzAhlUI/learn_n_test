4d676366f78f85ae8e1bdf0193a46c4e4c1b46ea
MoritzAhlUI <143088271+MoritzAhlUI@users.noreply.github.com>
2023-12-11 14:43:22 +0100

    Add logo

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
+from PIL import Image
 
 # Setup for Storytelling (matplotlib):
 plt.rcParams['font.family'] = 'monospace'
